640cc5883fd8b18c093199eb56cd4316f12e9357
mpro34 <mpro34@gmail.com>
2013-01-22 17:39:19 -0800

    Added the bottom half of the hexagon and changed it to brown

--- homework/canvas-basics/Brown_Hexagon/25e.js
+++ homework/canvas-basics/Brown_Hexagon/25e.js
@@ -1,16 +1,15 @@
 (function () {
     var canvas = document.getElementById("25e").getContext("2d");
-    canvas.fillStyle = "brown";
+    canvas.fillStyle = "#663300";
     canvas.beginPath();
     //top half
-    canvas.moveTo(150,100);
-    canvas.lineTo(200,50);
+    canvas.moveTo(100,150);
+    canvas.lineTo(150,50);
     canvas.lineTo(300,50);
-    canvas.lineTo(350,100);
+    canvas.lineTo(350,150);
     //bottom half
-    canvas.lineTo(200,50);
-    canvas.lineTo(300,50);
-    canvas.lineTo(350,100);
+    canvas.lineTo(300,250);
+    canvas.lineTo(150,250);
     canvas.closePath();
     //Draw outlined shape
     canvas.fill();
