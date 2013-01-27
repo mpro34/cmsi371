50c044b532a75b4e18d8767f4955eb08218a16c2
mpro34 <mpro34@gmail.com>
2013-01-27 13:12:01 -0800

    Created the dark blue sky

---
+++ homework/canvas-basics/Skyline/28e.js
@@ -0,0 +1,8 @@
+(function () {
+    var canvas = document.getElementById("28e").getContext("2d");
+    //dark blue sky
+    canvas.fillStyle = "#000047";
+    canvas.fillRect(0,0,600,500);
+
+  
+}());
