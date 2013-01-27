6f2719bec956a56f24c91f01e832b00e8cb02f41
mpro34 <mpro34@gmail.com>
2013-01-27 13:32:34 -0800

    Created a for loop to create random size and location buildings

--- homework/canvas-basics/Skyline/28e.js
+++ homework/canvas-basics/Skyline/28e.js
@@ -3,6 +3,14 @@
     //dark blue sky
     canvas.fillStyle = "#000047";
     canvas.fillRect(0,0,600,500);
+    canvas.fillStyle = "black";
+    //Function that draws random buildings and windows
+    for (var i=0; i<10; i++) {
+        var x=Math.floor(Math.random()*500);
+        var y=Math.floor(Math.random()*450); //Smallest building is 50 pixels
+        var w=Math.floor(60+Math.random()*200);
+        canvas.fillRect(x,y,w,600); //Want all buildings to reach the bottom
+    }
 
   
 }());
