a0b75061268498e668ad37201e32a1dbf38492dc
mpro34 <mpro34@gmail.com>
2013-01-28 11:27:55 -0800

    Added an algorithm to randomly generate windows through each building

--- homework/canvas-basics/Skyline/28e.js
+++ homework/canvas-basics/Skyline/28e.js
@@ -3,14 +3,21 @@
     //dark blue sky
     canvas.fillStyle = "#000047";
     canvas.fillRect(0,0,600,500);
-    canvas.fillStyle = "black";
     //Function that draws random buildings and windows
-    for (var i=0; i<10; i++) {
-        var x=Math.floor(Math.random()*500);
+    for (var i=0; i<600; i+=100) {
+        canvas.fillStyle = "black";
+        var x=i; //Generate a random sized building every 100 pixels
         var y=Math.floor(Math.random()*450); //Smallest building is 50 pixels
-        var w=Math.floor(60+Math.random()*200);
+        var w=Math.floor(80+Math.random()*150);
         canvas.fillRect(x,y,w,600); //Want all buildings to reach the bottom
+        canvas.fillStyle = "yellow";
+        var windowCount = Math.floor(Math.random()*(Math.floor(w/35)));
+        var size = 5; //Variable that continues the random window pattern down a building.
+        while (size <= 600) {
+            for (var j=0; j<=windowCount; j++) {
+                canvas.fillRect(x+5+(30*j),y+size,25,25);
+            }
+            size+=40;
+        }
     }
-
-  
 }());
