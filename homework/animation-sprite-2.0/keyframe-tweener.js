51c03dd431547d907b6beb5ead3eb42f8a4099f4
mpro34 <mpro34@gmail.com>
2013-02-04 11:43:05 -0800

    Further improved my custom background

--- homework/animation-sprite-2.0/keyframe-tweener.js
+++ homework/animation-sprite-2.0/keyframe-tweener.js
@@ -98,15 +98,22 @@ var KeyframeTweener = {
                 for (var i=0; i<55; i+=2) {
                     renderingContext.fillStyle = "#C2A385";
                     renderingContext.fillRect(100, 10*i, 800, 10);
+                    renderingContext.fillStyle = "#746250";
+                    renderingContext.fillRect(100, 10*i, 800, 4);
                     renderingContext.fillStyle = "#8A5C2E";
                     renderingContext.fillRect(100, 10*(i+1), 800, 10);
+                    renderingContext.fillStyle = "#C4AE96";
+                    renderingContext.fillRect(100, 10*(i+1), 800, 4);
                 }
+                renderingContext.fillStyle = "#5D4E40";
+                renderingContext.fillRect(187, 137, 626, 323);
                 renderingContext.fillStyle = "#E6E600";
                 renderingContext.fillRect(190, 140, 620, 320);
                 renderingContext.fillStyle = "#CCCC00";
                 renderingContext.fillRect(195, 145, 610, 310);
                 renderingContext.fillStyle = "#007ACC";
                 renderingContext.fillRect(200, 150, 600, 300);
+
             };
             background(renderingContext);
 
