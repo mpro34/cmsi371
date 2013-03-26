ec7575c53b73948d2a901dd96ce0eae7a703d1ec
mpro34 <mpro34@gmail.com>
2013-03-25 21:21:57 -0700

    Fixed spacing and deleted uneccessary comments

--- homework/pipeline/matrix4x4/matrix4x4.js
+++ homework/pipeline/matrix4x4/matrix4x4.js
@@ -10,7 +10,7 @@ var Matrix4x4 = (function () {
           0, 0, 0, 1 ];
     };
 
-    // matrix4x4 multiplication that takes in one matrix4x4 object.
+    //matrix4x4 multiplication that takes in one matrix4x4 object.
     matrix4x4.prototype.multiply = function (m2) {
         var result = new Matrix4x4(),
             i,
@@ -115,10 +115,10 @@ var Matrix4x4 = (function () {
     //t, b, l, and r are the top, bottom, left, and right distances of the viewport, respectively.
     //f and n are the far and near clipping planes, respectively.
         return new Matrix4x4(
-            2/(r-l),     0.0,      0.0, -(r+l)/(r-l),
-                0.0, 2/(t-b),      0.0, -(t+b)/(t-b),
-                0.0,     0.0, -2/(f-n), -(f+n)/(f-n),
-                0.0,     0.0,      0.0,          1.0
+            2/(r - l),       0.0,        0.0, -(r + l)/(r - l),
+                  0.0, 2/(t - b),        0.0, -(t + b)/(t - b),
+                  0.0,       0.0, -2/(f - n), -(f + n)/(f - n),
+                  0.0,       0.0,        0.0,              1.0
         );
     };
 
@@ -127,156 +127,13 @@ var Matrix4x4 = (function () {
     //t, b, l, and r are the top, bottom, left, and right distances of the viewport, respectively.
     //f and n are the far and near clipping planes, respectively.
         return new Matrix4x4(
-            (2*n)/(r-l),         0.0,  (r+l)/(r-l),            0.0,
-                    0.0, (2*n)/(t-b),  (t+b)/(t-b),            0.0,
-                    0.0,         0.0, -(f+n)/(f-n), -(2*n*f)/(f-n),
-                    0.0,         0.0,         -1.0,            0.0
+            (2 * n)/(r - l),             0.0,  (r + l)/(r - l),                  0.0,
+                        0.0, (2 * n)/(t - b),  (t + b)/(t - b),                  0.0,
+                        0.0,             0.0, -(f + n)/(f - n), -(2 * n * f)/(f - n),
+                        0.0,             0.0,             -1.0,                  0.0
         );
     };
 
-
-/**,
-    
-        // A private method for checking dimensions,
-        // throwing an exception when different.
-        checkDimensions = function (v1, v2) {
-            if (v1.dimensions() !== v2.dimensions()) {
-                throw "Vectors have different dimensions";
-            }
-        };
-
-    // Basic methods.
-    vector.prototype.dimensions = function () {
-        return this.elements.length;
-    };
-
-    vector.prototype.x = function () {
-        return this.elements[0];
-    };
-
-    vector.prototype.y = function () {
-        return this.elements[1];
-    };
-
-    vector.prototype.z = function () {
-        return this.elements[2];
-    };
-
-    vector.prototype.w = function () {
-        return this.elements[3];
-    };
-
-    // Addition and subtraction.
-    vector.prototype.add = function (v) {
-        var result = new Vector(),
-            i,
-            max;
-
-        // Dimensionality check.
-        checkDimensions(this, v);
-
-        for (i = 0, max = this.dimensions(); i < max; i += 1) {
-            result.elements[i] = this.elements[i] + v.elements[i];
-        }
-
-        return result;
-    };
-
-    vector.prototype.subtract = function (v) {
-        var result = new Vector(),
-            i,
-            max;
-
-        // Dimensionality check.
-        checkDimensions(this, v);
-
-        for (i = 0, max = this.dimensions(); i < max; i += 1) {
-            result.elements[i] = this.elements[i] - v.elements[i];
-        }
-
-        return result;
-    };
-
-    // Scalar multiplication and division.
-    vector.prototype.multiply = function (s) {
-        var result = new Vector(),
-            i,
-            max;
-
-        for (i = 0, max = this.dimensions(); i < max; i += 1) {
-            result.elements[i] = this.elements[i] * s;
-        }
-
-        return result;
-    };
-
-    vector.prototype.divide = function (s) {
-        var result = new Vector(),
-            i,
-            max;
-
-        for (i = 0, max = this.dimensions(); i < max; i += 1) {
-            result.elements[i] = this.elements[i] / s;
-        }
-
-        return result;
-    };
-
-    // Dot product.
-    vector.prototype.dot = function (v) {
-        var result = 0,
-            i,
-            max;
-
-        // Dimensionality check.
-        checkDimensions(this, v);
-
-        for (i = 0, max = this.dimensions(); i < max; i += 1) {
-            result += this.elements[i] * v.elements[i];
-        }
-
-        return result;
-    };
-
-    // Cross product.
-    vector.prototype.cross = function (v) {
-        // This method is for 3D vectors only.
-        if (this.dimensions() !== 3 || v.dimensions() !== 3) {
-            throw "Cross product is for 3D vectors only.";
-        }
-
-        // With 3D vectors, we can just return the result directly.
-        return new Vector(
-            (this.y() * v.z()) - (this.z() * v.y()),
-            (this.z() * v.x()) - (this.x() * v.z()),
-            (this.x() * v.y()) - (this.y() * v.x())
-        );
-    };
-
-    // Magnitude and unit vector.
-    vector.prototype.magnitude = function () {
-        // Make use of the dot product.
-        return Math.sqrt(this.dot(this));
-    };
-
-    vector.prototype.unit = function () {
-        // At this point, we can leverage our more "primitive" methods.
-        return this.divide(this.magnitude());
-    };
-
-    // Projection.
-    vector.prototype.projection = function (v) {
-        var unitv;
-
-        // Dimensionality check.
-        checkDimensions(this, v);
-
-        // Plug and chug :)
-        // The projection of u onto v is u dot the unit vector of v
-        // times the unit vector of v.
-        unitv = v.unit();
-        return unitv.multiply(this.dot(unitv));
-    };
-*/
     return matrix4x4;
+
 })();
